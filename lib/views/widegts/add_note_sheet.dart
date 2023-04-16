afb60fb47233b9d754e0395562eef48e6b6bae64
Mohamed Samy <m.samy.mahgoub97@gmail.com>
2023-04-16 05:31:16 +0200

    take input and validate data

--- lib/views/widegts/add_note_sheet.dart
+++ lib/views/widegts/add_note_sheet.dart
@@ -8,24 +8,63 @@ class AddNoteSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
-        Padding(
-          padding: EdgeInsets.only(top: 40, bottom: 16, left: 24, right: 24),
-          child: CustomTextField(
-            hint: 'Title',
+    return const AddNoteForm();
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({super.key});
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.only(top: 40, bottom: 16, left: 24, right: 24),
+            child: CustomTextField(
+              onSaved: (value) {
+                title = value;
+              },
+              hint: 'Title',
+            ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24),
-          child: CustomTextField(
-            hint: 'Content',
-            maxLines: 5,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: CustomTextField(
+              onSaved: (value) {
+                subTitle = value;
+              },
+              hint: 'Content',
+              maxLines: 5,
+            ),
           ),
-        ),
-        Spacer(),
-        CustomButtom(),
-      ],
+          const Spacer(),
+          CustomButtom(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/views/widegts/custom_buttom.dart
+++ lib/views/widegts/custom_buttom.dart
@@ -2,20 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
 
 class CustomButtom extends StatelessWidget {
-  const CustomButtom({super.key});
-
+  const CustomButtom({super.key, this.onTap});
+  final Function()? onTap;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
       child: GestureDetector(
+        onTap: onTap,
         child: Container(
           height: 50,
+          width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(8),
             color: kPrimaryColor,
           ),
-          width: double.infinity,
           child: const Center(
             child: Text(
               'Add',
